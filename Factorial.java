eb654e32714cf83a19f21fb21cb3b2918f48ef72
Alex Kapajika <alex.kapajika.ntumba@stu.ocsb.ca>
2025-04-24 23:07:05 +0000

    Factorial

---
+++ Factorial.java
@@ -0,0 +1,68 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+/**
+ * This program recursively calculates the factorial of each valid number from an input file
+ * and writes the result or error message for each line into an output file.
+ * Author: Alex Kapajika
+ * Version: 1.1
+ * Since: 2025-04-23
+ */
+public final class Factorial {
+    /** Constant for the input file. */
+    private static final String INPUT_FILE = "input.txt";
+    private static final String OUTPUT_FILE = "output.txt";
+    /**
+     * This is to satisfy the style checker.
+     *
+     * @exception IllegalStateException Utility class.
+     * @see IllegalStateException
+     */
+    private Factorial() {
+        throw new IllegalStateException("Utility class");
+    }
+    /**
+     * Main Method.
+     *
+     * @param args Unused.
+     */
+    public static void main(String[] args) {
+        // Pass the path to the file as a parameter
+        try (Scanner scanner = new Scanner(new File(INPUT_FILE));
+                FileWriter writer = new FileWriter(OUTPUT_FILE)) {
+            // Reading file line by line
+            while (scanner.hasNextLine()) {
+                // Read the line from the file
+                String line = scanner.nextLine().trim();
+                if (line.isEmpty()) {
+                    writer.write("Empty line.\n");
+                } else {
+                    // Check if the line is a valid number
+                    try {
+                        int number = Integer.parseInt(line);
+                        if (number < 0) {
+                            writer.write("Negative number.\n");
+                        } else {
+                            // Calculate the factorial
+                            int result = factorial(number);
+                            writer.write("Factorial of " + number + " is: "
+                             + result + "\n");
+                        }
+                    } catch (NumberFormatException e) {
+                        // Handle invalid number format
+                        writer.write("Invalid number : " + line + "\n");
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
